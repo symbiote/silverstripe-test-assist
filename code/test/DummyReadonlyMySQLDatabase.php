5d870d13a2219b743e67de0f9e9974a8340ba606
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-07-02 12:10:36 +1000

    NEW Dummy readonly MySQL class
    Added a new class that can be used for testing readonly mysql environments
    
    In a test class, you can now do
    
    ```
    	$newConf = $databaseConfig;
    	$newConf['type'] = "DummyReadonlyMySQLDatabase";
    	DB::connect($newConf);
    
    ```

---
+++ code/test/DummyReadonlyMySQLDatabase.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Description of DummyReadonlyMySQLDatabase
+ *
+ * @author marcus
+ */
+class DummyReadonlyMySQLDatabase extends MySQLDatabase {
+	public $writeQueries = array('insert','update','delete','replace', 'drop', 'create', 'truncate');
+	
+	public function query($sql, $errorLevel = E_USER_ERROR) {
+		if (in_array(strtolower(substr($sql,0,strpos($sql,' '))), $this->writeQueries)) {
+			throw new Exception("Attempted to write to readonly database");
+		}
+		return parent::query($sql, $errorLevel);
+	}
+}
